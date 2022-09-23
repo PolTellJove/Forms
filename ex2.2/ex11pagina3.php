0b53d176ffb317225b07aaca64d390b7c700f5e7
super <poltelljove@gmail.com>
2022-09-23 17:07:35 +0200

    ex22 i ex23 fets

---
+++ ex2.2/ex11pagina3.php
@@ -0,0 +1,14 @@
+<?php
+
+    $number = $_GET['number'];
+    echo "<h1>Iniciar comanda ".$number."</h1>";
+
+?>
+
+
+
+<!-- $quantity = $_POST['quantitat'];
+
+    for ($i = 1; $i < $quantity; $i++){
+        echo "<a href=''>Comanda "-$i."</a><br>";
+    } -->
---
+++ ex2.3/ex12pagina1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+    <?php
+        if($_GET['colors'] == 'Foc'){
+            echo "<body style = 'background-color:red'>";
+        }else if($_GET['colors'] == 'Aigua'){
+            echo "<body style = 'background-color:blue'>";
+        }else if($_GET['colors'] == 'Terra'){
+            echo "<body style = 'background-color:brown'>";
+        }else{
+            echo "<body>";
+        }
+    ?>
+
+    <form method="GET">
+        <select name="colors">
+            <option <?php if($_GET['colors']=='Foc')
+            echo "selected";?>>Foc</option>
+            <option <?php if($_GET['colors']=='Aigua')
+            echo "selected";?>>Aigua</option>
+            <option <?php if($_GET['colors']=='Terra')
+            echo "selected";?>>Terra</option>
+        </select>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
